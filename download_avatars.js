7b1b2dd0398515520eaa559c24d91fe125709eee
Arv <ArvNatarajan@gmail.com>
2016-09-13 17:17:19 -0400

    added package.json, and modified program to handle specified errors

--- download_avatars.js
+++ download_avatars.js
@@ -1,36 +1,15 @@
-const request = require("request");
-const fs = require("fs");
-const dotEnv = require('dotenv').config();
+const getRepoContributors = require("./lib/getRepoContributors");
+const downloadImageByURL = require("./lib/downloadImageByURL");
 
 
-function githubRequest(endPoint, callback) {
-  var requestData = {
-    url: `https://api.github.com${endPoint}`,
-    auth: {
-      bearer: process.env.DB_TOKEN
-    },
-    headers: {
-      "User-Agent": "ABCD"
-    }
+
+function checkCommandLineArgumentsThenRun() {
+  //Error to check if the right number of arguments have been input to CLI
+  if (process.argv.length !== 4) {
+    console.log("Incorrect number of arguments! Please ensure you have entered in Owner and Repo name");
+  } else {
+    getRepoContributors(process.argv[2], process.argv[3], downloadImageByURL);
   }
-  request.get(requestData, callback);
 }
 
-function getRepoContributors(repoOwner, repoName, callback) {
-  githubRequest(`/repos/${repoOwner}/${repoName}/contributors`, (err, response, body) => {
-    if (err) {
-      throw err;
-    }
-    var data = JSON.parse(body)
-
-    data.forEach( (item) => {
-      callback(item.avatar_url, item.login);
-    })
-  })
-}
-
-function downloadImagebyURL(url, path) {
-  request(url).pipe(fs.createWriteStream(`./avatars/${path}.png`))
-}
-
-getRepoContributors(process.argv[2], process.argv[3], downloadImagebyURL);
+checkCommandLineArgumentsThenRun();
